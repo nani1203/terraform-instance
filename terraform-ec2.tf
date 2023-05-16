1d8ac321857e2753b4d7f72546c9ff198373b314
nani1203 <89770120+nani1203@users.noreply.github.com>
2023-05-16 10:42:34 +0530

    Update terraform-ec2.tf

--- terraform-ec2.tf
+++ terraform-ec2.tf
@@ -1,22 +1,15 @@
 provider "aws" {
   profile    = "terraform_user-1"
-  access_key = "AKIA4BHZHQQA3TQAXW5F"
-  secret_key = "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"
+  access_key = "AKIA4BHZHQQA4JLVEQOV"
+  secret_key = "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"
   region     = "ap-south-1"
 }
-resource "aws_instance" "terraform" {
-  ami             = "ami-07d3a50bd29811cd1"
-  instance_type   = "t2.micro"
-  key_name        = "terraform_server"
-  security_groups = ["${aws_security_group.terraformdemoSG.name}"]
-}
-resource "aws_security_group" "terraformdemoSG" {
-  name        = "terraformdemoSG"
-  description = "terraformdemoSG"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_instance" "demo-server" {
+  ami                    = "ami-0b08bfc6ff7069aff"
+  key_name               = "Terraform"
+  instance_type          = "t2.micro"
+  tags = {
+    Name = "terraformdemo-1"
   }
 }
+
