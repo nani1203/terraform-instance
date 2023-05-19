9469c00175afbcfe90323a5763f51648b3e671ac
nani1203 <89770120+nani1203@users.noreply.github.com>
2023-05-19 11:16:06 +0530

    remove the Access Keys

--- terraform-ec2.tf
+++ terraform-ec2.tf
@@ -1,15 +1,10 @@
-provider "aws" {
-  profile    = "terraform_user-1"
-  access_key = "AKIA4BHZHQQA4JLVEQOV"
-  secret_key = "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"
-  region     = "ap-south-1"
-}
-resource "aws_instance" "demo-server" {
+
+resource "aws_instance" "demo-creation" {
   ami                    = "ami-0b08bfc6ff7069aff"
   key_name               = "Terraform"
   instance_type          = "t2.micro"
   tags = {
-    Name = "terraformdemo-1"
+    Name = "terraformdemo-3"
   }
 }
 
